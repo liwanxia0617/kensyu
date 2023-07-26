875707ab45ac06328c04e8435f91d63b8b75970a
liwanxia0617 <liwanxia0617@yahoo.ne.jp>
2023-07-27 04:54:37 +0900

    test

---
+++ main.py
@@ -0,0 +1,5 @@
+x = 15
+if 10 <= x <= 20:
+    print(x)
+
+print(f'apple {x}')
---
+++ oldhello.py
@@ -0,0 +1,10 @@
+#def greeting(name):
+    #print("Hello,",name)
+
+#greeting("World")
+
+def main():
+    print("Hello World")
+
+if __name__== "__main__":
+    main()
