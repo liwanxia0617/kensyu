fbb6bd1004389e15c38a91429cf89605fd10817e
liwanxia0617 <liwanxia0617@yahoo.ne.jp>
2023-08-01 18:03:02 +0900

    test

--- palindrome/palindrome.py
+++ palindrome/palindrome.py
@@ -1,15 +1,13 @@
-from collections import defaultdict
 import re
 
 path = "./in.txt"
-words = defaultdict(int)
 with open("out.csv", mode='w', encoding="utf-8") as writer:
     print("No,回文の文書,回文(かな),判定", file=writer)
     writer.close()
 with open(path,'r+',encoding = 'utf-8') as reader:
     for i, element in enumerate(reader):
         j = i + 1
-        atama = str(j) + ","
+        atama = str(j) + ","# pylint: disable=C0103
         for word in element.strip().split(' '):
             word = re.sub(r"\（",",",word)
             word1 = re.sub(r"\）",",",word)
