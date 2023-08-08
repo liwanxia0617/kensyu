1b1d100e8662a6d1a78e4ef4be986cd3b1759740
liwanxia0617 <liwanxia0617@yahoo.ne.jp>
2023-08-08 19:26:58 +0900

    palindrome commit

--- palindrome/palindrome.py
+++ palindrome/palindrome.py
@@ -1,10 +1,11 @@
 """ 指定されたひらがなの部分の回文判断プログラム """
-with open("in.txt","r",encoding="utf-8") as reader,open("out.csv","w",encoding="shift_jis") as writer:
-    for no,line in enumerate(reader):
-        no = no + 1
-        text, sentence = line.strip().replace("）", "").split("（")
-        if sentence == sentence[::-1]:
-            result = "〇"  # pylint: disable=C0103
-        else:
-            result = "×"  # pylint: disable=C0103
-        print(f"{no},{text},{sentence},{result}",file=writer)  # pylint: disable=C0304
+with open("in.txt","r",encoding="utf-8") as reader:
+    with open("out.csv","w",encoding="shift_jis") as writer:
+        for no,line in enumerate(reader):
+            no = no + 1
+            text, sentence = line.strip().replace("）", "").split("（")
+            if sentence == sentence[::-1]:
+                result = "〇"  # pylint: disable=C0103
+            else:
+                result = "×"  # pylint: disable=C0103
+            print(f"{no},{text},{sentence},{result}",file=writer)  # pylint: disable=C0304
