808d153c08b4c6a1c602fa9845cad9b8ca2ece93
liwanxia0617 <liwanxia0617@yahoo.ne.jp>
2023-08-08 18:53:08 +0900

    test

--- palindrome/palindrome.py
+++ palindrome/palindrome.py
@@ -2,23 +2,9 @@
 with open("in.txt","r",encoding="utf-8") as reader,open("out.csv","w",encoding="utf-8") as writer:
     for no,line in enumerate(reader):
         no = no + 1
-<<<<<<< HEAD
         text, sentence = line.strip().replace("）", "").split("（")
         if sentence == sentence[::-1]:
             result = "〇"  # pylint: disable=C0103
         else:
             result = "×"  # pylint: disable=C0103
         print(f"{no},{text},{sentence},{result}",file=writer)  # pylint: disable=C0304
-=======
-        for line in line.split():
-            line = line.replace("（", ":")
-            line = line.replace("）", ":")
-            line = line.split(":")
-            text = line[0]
-            sentence = line[1]
-            if sentence == sentence[::-1]:
-                result = "〇"  # pylint: disable=C0103
-            else:
-                result = "×"  # pylint: disable=C0103
-            print(f"{no},{text},{sentence},{result}",file=writer)  # pylint: disable=C0304
->>>>>>> 755042b0ed26b8b731318ac2e170a94c2fa99303
